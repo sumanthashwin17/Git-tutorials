e777da825b8449a3589adb21e34c1b22d7b0873a
sumanthashwin17 <sumanthashwin24@gmail.com>
2026-07-16 18:27:11 +0530

    version 19

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('config.js');
-console.log('version-1 of git in config.js');
+console.log('version-1 and 2 and 3 of git in config.js');
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 console.log('index.js');
-console.log('version-1 of git in index.js')
+console.log('version-1 and 2 and 3 of git in index.js');
